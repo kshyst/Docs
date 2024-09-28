37577db4f1643b7b34ef099cc7ebd30da8c4c4d9
kshyst <kiarash.sh1383@gmail.com>
2024-09-28 10:32:02 +0330

    Update

--- git.c
+++ git.c
@@ -1,10 +1,8 @@
 #include <stdlib.h>
 
 int main() {
-    
-    system("cd S:/Docs");
     system("git add .");
     system("git commit -m \"Update\"");
     system("git push 1 main");
     return 0;
-}
+} 
